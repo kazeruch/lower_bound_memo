8b51f0a556ce55d5f076332994873d2573626a3c
kazeruch <kazukirock0417@gmail.com>
2024-03-21 19:08:29 +0900

    removed unused libraries, add example

--- lower_bound_memo.cpp
+++ lower_bound_memo.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -16,10 +15,17 @@ int main() {
     //lower_bound と upper_bound 関数は使う前に sort する必要性あり
 
     // x 以上の値の中での最小値を取得する。
-    cout << *lower_bound(a.begin(), a.end(), x) << endl;
+    cout << *lower_bound(a.begin(), a.end(), x) << endl; // [1]
 
     // x 以上の値の中での最小値の中でイテレータが最も小さいものを取得する。
-    cout << lower_bound(a.begin(), a.end(), x) - a.begin() << endl;
+    cout << lower_bound(a.begin(), a.end(), x) - a.begin() << endl; // [2]
+
+    // example
+    // n = 15, x = 47
+    // a = { 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67 }
+
+    // [1] -> 47
+    // [2] -> 10
 
     return 0;
 }
